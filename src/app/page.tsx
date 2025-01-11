98e05ccacc939754e4fe5f3358eb31649f50ad29
Taufique-Speqto <taufique.ali@speqto.com>
2025-01-12 02:29:52 +0530

    page router pratice

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum alias vitae tenetur ipsum reprehenderit nobis expedita, eos excepturi facere beatae animi iusto sapiente rerum dolorem facilis natus necessitatibus, iure molestiae?
+    </div>
+  );
+}
